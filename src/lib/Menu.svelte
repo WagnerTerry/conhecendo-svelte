831ff3b91c9fca364f4fd6f081e27aaba5c6f07c
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-19 15:31:23 -0300

    feat: iniciando com estruturas lógicas no svelte

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -2,17 +2,24 @@
 
   import Logo from "./Logo.svelte";
 
+  const items = [
+    { href: '/', label: 'Início'},
+    { href: '/sobre', label: 'Sobre'},
+    { href: '/portfolio', label: 'Portfólio'},
+    { href: '/contato', label: 'Contato'},
+    { href: '/faq', label: 'Perguntas Frequentes'},
+
+  ]
+
 </script>
 <a href="/">
     <Logo />
 </a>
 
-<a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
+{#each items as {href, label}}
 
-<a href="/sobre" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Sobre</a>
+<a {href} class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 hover:border-blue-500 mx-1.5 sm:mx-6">{label}</a>
 
-<a href="/portfolio" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Portfolio</a>
+{/each}
+<!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a> -->
 
-<a href="/contato" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contato</a>
-
-<a href="/faq" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Perguntas Frequentes</a>
