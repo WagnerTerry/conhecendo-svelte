040f91f74298de55d8ff52646b5711a36c1d0e94
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-18 22:07:42 -0300

    feat: Adicionando Bibliotecas com Svelte-Add

--- README.md
+++ README.md
@@ -36,3 +36,9 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+## Adicionando Bibliotecas com Svelte-Add
+- https://github.com/svelte-add/svelte-add?tab=readme-ov-file
+
+## Dependencies
+- npx svelte-add@latest tailwindcss --forms --typography
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
+<script>
+  import "../app.pcss";
+</script>
+
 <h1>Layout</h1>
 <ul>
-    <li><a href="/">Inicio</a></li>
-    <li><a href="/sobre">Sobre</a></li>
-    <li><a href="/portfolio">Portfolio</a></li>
-    <li><a href="/contato">Contato</a></li>
-    <li><a href="/faq">Perguntas Frequentes</a></li>
-
+  <li><a href="/">Inicio</a></li>
+  <li><a href="/sobre">Sobre</a></li>
+  <li><a href="/portfolio">Portfolio</a></li>
+  <li><a href="/contato">Contato</a></li>
+  <li><a href="/faq">Perguntas Frequentes</a></li>
 </ul>
 
 <slot />
