dbf5e5a534973c43a4e8ca381f357fa0423970b4
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-18 21:34:52 -0300

    feat: rotas e layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
-<h1>Meu Site</h1>
-
+<h1>Layout</h1>
 <ul>
     <li><a href="/">Inicio</a></li>
     <li><a href="/sobre">Sobre</a></li>
     <li><a href="/portfolio">Portfolio</a></li>
     <li><a href="/contato">Contato</a></li>
 </ul>
+
+<slot />
