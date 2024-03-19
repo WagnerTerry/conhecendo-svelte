6cf6111df88736b7ecc19cd63ac3843d7f72c2ef
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-18 22:25:46 -0300

    feat: iniciando componente

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
+<script>
+    import Button from "./Button.svelte";
+</script>
+
 <h1>Meu Site</h1>
 
-<ul>
+<Button label="Clique aqui" />
+<Button />
+
+<!-- <ul>
     <li><a href="/">Inicio</a></li>
     <li><a href="/sobre">Sobre</a></li>
     <li><a href="/portfolio">Portfolio</a></li>
     <li><a href="/contato">Contato</a></li>
     <li><a href="/faq">Perguntas Frequentes</a></li>
-
-</ul>
+</ul> -->
---
+++ src/routes/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let label = 'Botão'
+</script>
+
+<button class="btn">{label}</button>
+
+<style>
+    .btn {
+        color: #f00;
+    }
+</style>
