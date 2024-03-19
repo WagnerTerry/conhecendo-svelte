2b6fe6e03722ca25b8503730f20b54963e84bd13
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-19 16:14:10 -0300

    feat; Conhecendo os Svelte Stores

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,25 +1,32 @@
 <script>
-
+  import { page } from "$app/stores";
   import Logo from "./Logo.svelte";
 
   const items = [
-    { href: '/', label: 'Início'},
-    { href: '/sobre', label: 'Sobre'},
-    { href: '/portfolio', label: 'Portfólio'},
-    { href: '/contato', label: 'Contato'},
-    { href: '/faq', label: 'Perguntas Frequentes'},
-
-  ]
-
+    { href: "/", label: "Início" },
+    { href: "/sobre", label: "Sobre" },
+    { href: "/portfolio", label: "Portfólio" },
+    { href: "/contato", label: "Contato" },
+    { href: "/faq", label: "Perguntas Frequentes" },
+  ];
 </script>
+
 <a href="/">
-    <Logo />
+  <Logo />
 </a>
 
-{#each items as {href, label}}
-
-<a {href} class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 hover:border-blue-500 mx-1.5 sm:mx-6">{label}</a>
-
+{#each items as { href, label }}
+  {#if $page.url.pathname === href}
+    <a
+      {href}
+      class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+      >{label}</a
+    >
+  {:else}
+    <a
+      href={href}
+      class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+      >Home</a
+    >
+  {/if}
 {/each}
-<!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a> -->
-
